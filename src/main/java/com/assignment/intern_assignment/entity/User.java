b00edd5554cdf50ceb73a470eb4c58bd6add097a
NayabJalal <jmd341602@gmail.com>
2026-05-08 22:10:06 +0530

    first commit

---
+++ src/main/java/com/assignment/intern_assignment/entity/User.java
@@ -0,0 +1,16 @@
+package com.assignment.intern_assignment.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+}
---
+++ src/test/java/com/assignment/intern_assignment/InternAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.intern_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
